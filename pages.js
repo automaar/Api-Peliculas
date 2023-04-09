35c0b3d54b75aab3c61ecd64ae1b21e84b822910
Marcos <117835411+automaar@users.noreply.github.com>
2023-04-09 17:19:14 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -2,22 +2,73 @@
 
 var numPag = document.getElementById('selectPage').value;
 var root = document.getElementById('root')
+const paginas = document.getElementById('pages')
+
+numPag=1;
+var num = numPag;
+
+async function paginaAnterior() {
+    if (num == 1){
+        return "no se puede ver menos paginas que 0"
+    }
+    num = num-1;
+    const numPag  = num;
+    console.log('numero pag', numPag)
+    paginas.innerHTML=`
+        <nav aria-label="Page navigation example" id="pages">
+            <ul class="pagination justify-content-center">
+                <li class="page-item ">
+                    <button class="page-link" onclick="paginaAnterior()">Previous</button>
+                </li>
+                <li class="page-item" id="selectPage"><p  class="page-link">${numPag}</p></li>
+                    <li class="page-item">
+                    <button class="page-link" onclick="paginaSiguiente()">Next</button>
+                </li>
+            </ul>
+        </nav>`;
+    verPaginas()
+}
+
+async function paginaSiguiente(){
+    num = num+1;
+    const numPag =+ num;
+    console.log('numero pag', numPag)
+    paginas.innerHTML=`
+        <nav aria-label="Page navigation example" id="pages">
+            <ul class="pagination justify-content-center">
+                <li class="page-item ">
+                    <button class="page-link" onclick="paginaAnterior()">Previous</button>
+                </li>
+                <li class="page-item" id="selectPage"><p  class="page-link">${numPag}</p></li>
+                    <li class="page-item">
+                    <button class="page-link" onclick="paginaSiguiente()">Next</button>
+                </li>
+            </ul>
+        </nav>`;
+    verPaginas()
+}
 
 
+console.log('numero pag', num)
 
+async function verPaginas(){
+    root.innerHTML=`
+    <div class="spinner-grow text-secondary" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>`;
 
-
-async function verPaginas(numPag){
     
 
-    const root = await fetch(`http://www.omdbapi.com/?s=none&page=${numPag}&apikey=221ba3f5`)
-    
-    
-    let rootjson = await root.json()
-    
-    let peliculasSearch = rootjson.Search
+    console.log(numPag);
+    const data = await fetch(`http://www.omdbapi.com/?s=none&page=${numPag}&apikey=221ba3f5`)
+    console.log(numPag);
 
-    root.innerHTML=''
+    let datajson = await data.json()
+    
+    let peliculasSearch = datajson.Search
+
+    root.innerHTML='';
+
     for (let i = 0; i < peliculasSearch.length; i++) {
         const pelicula = peliculasSearch[i];
         
@@ -26,45 +77,27 @@ async function verPaginas(numPag){
         const año = pelicula.Year
         const tipo = pelicula.Type
         const portada = pelicula.Poster
-        
-        
+        const id = pelicula.imdbID
 
-
+        let dataID = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=221ba3f5`);
+        let idjson = await dataID.json();
+        const genero = idjson.Genre;
+        const director = idjson.Director;
+        
         const peliculaHTMl = `
-            <div class="card" style="width: 18rem;">
-                <img src="${portada}" class="card-img-top" alt="...">
+            <div class="card" style="width: 15rem;">
+                <img src="${portada}"  width="236px" height="350px">
                 <div class="card-body">
-                    <h5 class="card-title">${titulo}</h5>
-                    <p class="card-text">${año}</p>
-                    <p class="card-text">${tipo}</p>
-                    <a href="#" class="btn btn-primary">More</a>
+                    <h5class="card-title">${titulo}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">Director: ${director}</h6>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">${año}</h6>
+                    <p><br>${tipo}</p>
+                    <p>${genero}</p>
+                    
                 </div>
             </div>
             `;
         
-
-        
-        
         root.innerHTML += peliculaHTMl;
     }
-    
-
-
-
-    console.log(peliculasSearch);
-
 }
-
-async function paginaAnterior() {
-    const numPage = numPag-1;
-    console.log(numPage);
-}
-
-
-async function paginaSiguiente(){
-    const numPage = numPag+1;
-    console.log(numPage);
-}
-
-
-console.log(numPag);
