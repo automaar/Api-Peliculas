99077432cff280bf722b68068d253ed166d2b675
Marcos <117835411+automaar@users.noreply.github.com>
2023-04-09 17:19:47 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,27 +1,69 @@
 
 var root = document.getElementById('root')
+var BPnombre = document.getElementById('buscarPorNombre')
+var idPages = document.getElementById('pages')
 
-export async function buscarPorNombre(nombrePelicula){
-    const data = await fetch(`http://www.omdbapi.com/?s=${nombrePelicula}&apikey=221ba3f5`);
+console.log('nombre: ',  BPnombre);
 
-    const caracteristicas = await data.json();
+BPnombre.addEventListener("change", async(event)=>{
+    root.innerHTML='';
+    let nombre = event.target.value;
+    await buscarPorNombre(nombre);
+})
 
-    const titulo = caracteristicas.Title;
-    const imagen = caracteristicas.Poster;
-    const año = caracteristicas.Year;
 
-    const peliHTML = `
-        <div class="card" style="width: 18rem;">
-            <img src="${imagen}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${titulo}</h5>
-                <p class="card-text">${año}</p>
-                <a href="#" class="btn btn-primary">More</a>
+
+async function buscarPorNombre(nombre){
+    idPages.innerHTML='';
+
+    root.innerHTML=`
+    <div class="spinner-grow text-secondary" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>`;
+
+    
+    const data = await fetch(`http://www.omdbapi.com/?s=${nombre}&apikey=221ba3f5`);
+    
+
+    let datajson = await data.json()
+    
+    let peliculasSearch = datajson.Search
+
+    root.innerHTML='';
+
+    console.log(peliculasSearch);
+    for (let i = 0; i < peliculasSearch.length; i++) {
+        const pelicula = peliculasSearch[i];
+        
+        
+        const titulo = pelicula.Title
+        const año = pelicula.Year
+        const tipo = pelicula.Type
+        const portada = pelicula.Poster
+        const id = pelicula.imdbID
+
+        let dataID = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=221ba3f5`);
+        let idjson = await dataID.json();
+        const genero = idjson.Genre;
+        const director = idjson.Director;
+        
+        const peliculaHTMl = `
+            <div class="card" style="width: 15rem;">
+                <img src="${portada}"  width="236px" height="350px">
+                <div class="card-body">
+                    <h5class="card-title">${titulo}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">Director: ${director}</h6>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">${año}</h6>
+                    <p><br>${tipo}</p>
+                    <p>${genero}</p>
+                    
+                </div>
             </div>
-        </div>
-        `;
-
-    root.innerHTML += peliHTML;
+            `;
+        
+        root.innerHTML += peliculaHTMl;
+    }
+    
 }
 
 
